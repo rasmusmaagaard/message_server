5f833028ff5c69ec6c1c06e8c4c5b878c2655532
Rasmus Maagaard <rasmusmaagaard@mac.com>
2019-11-17 00:04:32 +0100

    New feature. Use Action Cable channel when creating new messages.

--- message_server/app/javascript/channels/room_channel.js
+++ message_server/app/javascript/channels/room_channel.js
@@ -1,6 +1,6 @@
  import consumer from "./consumer"
 
-consumer.subscriptions.create("RoomChannel", {
+let room = consumer.subscriptions.create("RoomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
   },
@@ -9,12 +9,40 @@ consumer.subscriptions.create("RoomChannel", {
     // Called when the subscription has been terminated by the server
   },
 
-  received(data) {
+  received(message) {
     // Called when there's incoming data on the websocket for this channel
+    $('#messages').prepend('<div><b>'+message.sender+'</b> <i>'+message.content+'</i></div>');
 
-    $('#message_list').prepend('<li>' + data.content + '</li>');
+    // Debugging pad
+    console.log(message);
+  },
 
-    // Debuging pad
-    console.log(data);
-  }
+  broadcast(message) {
+    // Calls RoomChannel#broadcast on the server.
+    this.perform("broadcast", message)
+  },
+});
+
+let send_message = function() {
+  let message_content = $('#message_content');
+  let content = message_content.val();
+
+  room.broadcast({ sender: 'Tardis', content: content });
+  message_content.val('');
+};
+
+// Quick and dirty Unobtrusive JavaScript :)
+$(document).on('turbolinks:load', function () {
+  // The send button will send the message
+  $('#send_message').on('click', function() {
+    send_message()
+  });
+
+  // Pressing the return key will also send the message
+  $('#message_content').on('keypress', function(event) {
+    if (event.keyCode ===  13) { // 13 is return/enter
+      send_message();
+      event.preventDefault();
+    }
+  });
 });
