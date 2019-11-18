16907d6b87c36f54416b11c13b456564a14938f5
Rasmus Maagaard <rasmusmaagaard@mac.com>
2019-11-18 11:01:45 +0100

    Documentation. Add code comments to clarify code.

--- message_server/app/javascript/channels/room_channel.js
+++ message_server/app/javascript/channels/room_channel.js
@@ -1,3 +1,5 @@
+// Here we handle the Action Cable RoomChannel.
+// TODO: Currently this is tightly coupled to the "Tardis" page. This should be refactored if the room channel is to be used from multiple pages.
  import consumer from "./consumer"
 
  var message_template_source;
@@ -14,6 +16,9 @@
 
   received(message) {
     // Called when there's incoming data on the websocket for this channel
+
+    // Use handlebar template to create a new speechbubble and insert it below the existing messages.
+    // After adding a new message we scroll the the bottom of the page to make it visible.
     $('#messages').append(message_template(message));
     $('html, body').animate({scrollTop:$(document).height()}, 'slow');
 
@@ -27,6 +32,9 @@
   },
 });
 
+// This helper method is called when the user press enter or use the send button.
+// We get the content from the input field and send it.
+// TODO: We are currently hardcoding the username "Tardis" and a orange color. The user should be able to choose it from the message interface.
 let send_message = function() {
   let message_content = $('#message_content');
   let content = message_content.val();
@@ -37,7 +45,7 @@ let send_message = function() {
 
 // Quick and dirty Unobtrusive JavaScript :)
 $(document).on('turbolinks:load', function () {
-  // The send button will send the message
+  // The "Send" button used when sending a new message
   $('#send_message').on('click', function() {
     send_message()
   });
@@ -50,7 +58,7 @@ $(document).on('turbolinks:load', function () {
     }
   });
 
-  // We can first load the handlebar template after the load
+  // Setup handlebar. We can first access the template after the page has load (using rails turbo links)
   message_template_source = document.getElementById("message-template").innerHTML;
   message_template = Handlebars.compile(message_template_source);
 });
